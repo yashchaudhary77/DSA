6b2352918033c617485198e30d9f75a593b020f1
yashchaudhary77 <chaudharyyash6996@gmail.com>
2025-10-02 10:47:51 +0530

    to find odd or even number

---
+++ odd_number.java
@@ -0,0 +1,21 @@
+// Java program to Find Odd or Even number 
+
+import java.util.Scanner;
+
+public class odd_number {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n%2==0){
+            System.out.println("even number");
+
+        }
+        else{
+            System.out.println("odd number");
+        }
+
+
+    }
+
+    
+}
