9d68f8263aa20baf6c111c4672b917b1cd3a5cb6
yashchaudhary77 <chaudharyyash6996@gmail.com>
2025-10-08 21:56:05 +0530

    to add the digit of number

---
+++ sum0fDigit.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class sum0fDigit {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum=0;
+        while(n != 0){
+            sum = sum+n%10 ;
+            n = n/10;
+
+
+        }
+        System.out.println(sum);
+
+    }
+    
+}
