f6e7f2fe92177ac278163ef1484616f95c10d107
yashchaudhary77 <chaudharyyash6996@gmail.com>
2025-10-08 22:12:07 +0530

    palindrome

---
+++ palindrome.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class palindrome {
+    public static int pal(int n){
+        int rev = 0;
+        int temp = n;
+        while(temp != 0){
+            rev = rev*10 + temp%10;
+            temp = temp/10;
+        }
+        return rev;
+
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            int result = pal(n);
+            System.out.println(result);
+
+        
+    }
+    
+}
